bc8a5d70be09b3ac6b075b6243b2451477ebae73
alvaro javier garcia <alvajacobacci@hotmail.com>
2024-11-07 16:58:33 -0300

    laboratorio4

---
+++ script.js
@@ -0,0 +1,8 @@
+const backToTop = document.querySelector('.back-to-top');
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
